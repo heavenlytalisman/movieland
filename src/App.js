d56491195d5f34b5a119d863c6be90caf08b8368
heavenlytalisman <173621005+heavenlytalisman@users.noreply.github.com>
2025-09-14 10:28:50 +0530

    Fix: fix the api link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import searchIcon from './search.svg';
 import MovieCard from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=8953c528";
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=8953c528";
 
 const App = () => {
     const [movies, setMovies] = useState([]);
